986ac1d1b1f79fce8e690a4b647dd9aefa0e7f85
BiglandUK <bigsoft@hotmail.co.uk>
2019-06-12 19:45:18 +0100

    Comments added

--- WhiteBoard-Proto-Stage-1/ObjectManager.cpp
+++ WhiteBoard-Proto-Stage-1/ObjectManager.cpp
@@ -4,8 +4,10 @@
 ObjectManager::ObjectManager() 
 	:systemMovement(mObjects)
 {
+	// Can move around with mouse - but will stop when friction overcomes it
 	mObjects.push_back(std::make_shared<Object>(Object(0.f, 0.f))); mObjects[0]->SetFillColour(sf::Color::Red);
 
+	// Will move via motor, but stop when it hits the edge
 	mObjects.push_back(std::make_shared<Object>(Object(1000.f, 500.f))); mObjects[1]->SetFillColour(sf::Color::Yellow);
 	mObjects[1]->AddMotor();
 	mObjects[1]->GetProperty<ObjectMovement>(Property::Move)->SetBounce(false);
@@ -14,14 +16,15 @@ ObjectManager::ObjectManager()
 	pm->ChangeType(PropulsionMotor::Type::Constant);
 	pm->SetSpeed(400.0f);
 
-	mObjects.push_back(std::make_shared<Object>(Object(250.f, 300.f))); mObjects[2]->SetFillColour(sf::Color::Green); //mObjects[2]->ToggleVisibility();
+	// Moves via motor, but bounces when hits the edge
+	mObjects.push_back(std::make_shared<Object>(Object(250.f, 300.f))); mObjects[2]->SetFillColour(sf::Color::Green);
 	mObjects[2]->AddMotor();
 	std::shared_ptr<PropulsionMotor> pm3 = mObjects[2]->GetProperty<PropulsionMotor>(Property::Motor);
 	pm3->SetSpeed(400.0f);
-	pm3->SetAcceleration(40.f);
+	pm3->SetAcceleration(120.f);
 	pm3->ChangeType(PropulsionMotor::Type::Constant);
 
-	
+	// Moves using a pulsing motor
 	mObjects.push_back(std::make_shared<Object>(Object(700.f, 25.f))); mObjects[3]->SetFillColour(sf::Color::Blue);
 	mObjects[3]->AddMotor();
 	std::shared_ptr<PropulsionMotor> pm2 = mObjects[3]->GetProperty<PropulsionMotor>(Property::Motor);
